6452ae37ba9dff2a4080326a8b89a009080c6d9c
[your <00222320@uca.edu.sv>
2021-06-21 15:44:54 -0600

    Added solution folder, included login form

---
+++ bdPooFinalPj/bdPooFinalPj/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace bdPooFinalPj
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            this.pnlLogin1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlLogin2 = new System.Windows.Forms.Panel();
+            this.pnlCabain = new System.Windows.Forms.Panel();
+            this.cboCabain = new System.Windows.Forms.ComboBox();
+            this.lblCabain = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.pnlPassword = new System.Windows.Forms.Panel();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.pnlUsername = new System.Windows.Forms.Panel();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnlLogin1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlLogin2.SuspendLayout();
+            this.pnlCabain.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlLogin1
+            // 
+            this.pnlLogin1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.pnlLogin1.Controls.Add(this.label2);
+            this.pnlLogin1.Controls.Add(this.label1);
+            this.pnlLogin1.Controls.Add(this.pictureBox2);
+            this.pnlLogin1.Controls.Add(this.pictureBox1);
+            resources.ApplyResources(this.pnlLogin1, "pnlLogin1");
+            this.pnlLogin1.Name = "pnlLogin1";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnlLogin2
+            // 
+            this.pnlLogin2.BackColor = System.Drawing.Color.White;
+            this.pnlLogin2.Controls.Add(this.pnlCabain);
+            this.pnlLogin2.Controls.Add(this.panel1);
+            this.pnlLogin2.Controls.Add(this.lblPassword);
+            this.pnlLogin2.Controls.Add(this.pnlPassword);
+            this.pnlLogin2.Controls.Add(this.txtPassword);
+            this.pnlLogin2.Controls.Add(this.lblUsername);
+            this.pnlLogin2.Controls.Add(this.pnlUsername);
+            this.pnlLogin2.Controls.Add(this.txtUsername);
+            this.pnlLogin2.Controls.Add(this.lblLogin);
+            resources.ApplyResources(this.pnlLogin2, "pnlLogin2");
+            this.pnlLogin2.Name = "pnlLogin2";
+            // 
+            // pnlCabain
+            // 
+            this.pnlCabain.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pnlCabain.Controls.Add(this.cboCabain);
+            this.pnlCabain.Controls.Add(this.lblCabain);
+            resources.ApplyResources(this.pnlCabain, "pnlCabain");
+            this.pnlCabain.Name = "pnlCabain";
+            // 
+            // cboCabain
+            // 
+            this.cboCabain.BackColor = System.Drawing.Color.LightSkyBlue;
+            resources.ApplyResources(this.cboCabain, "cboCabain");
+            this.cboCabain.ForeColor = System.Drawing.Color.Gray;
+            this.cboCabain.FormattingEnabled = true;
+            this.cboCabain.Name = "cboCabain";
+            // 
+            // lblCabain
+            // 
+            resources.ApplyResources(this.lblCabain, "lblCabain");
+            this.lblCabain.Name = "lblCabain";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSkyBlue;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // lblPassword
+            // 
+            resources.ApplyResources(this.lblPassword, "lblPassword");
+            this.lblPassword.BackColor = System.Drawing.Color.Transparent;
+            this.lblPassword.ForeColor = System.Drawing.Color.Silver;
+            this.lblPassword.Name = "lblPassword";
+            // 
+            // pnlPassword
+            // 
+            this.pnlPassword.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.pnlPassword, "pnlPassword");
+            this.pnlPassword.Name = "pnlPassword";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.White;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.txtPassword, "txtPassword");
+            this.txtPassword.Name = "txtPassword";
+            // 
+            // lblUsername
+            // 
+            resources.ApplyResources(this.lblUsername, "lblUsername");
+            this.lblUsername.BackColor = System.Drawing.Color.Transparent;
+            this.lblUsername.ForeColor = System.Drawing.Color.Silver;
+            this.lblUsername.Name = "lblUsername";
+            // 
+            // pnlUsername
+            // 
+            this.pnlUsername.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.pnlUsername, "pnlUsername");
+            this.pnlUsername.Name = "pnlUsername";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.Color.White;
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.txtUsername, "txtUsername");
+            this.txtUsername.Name = "txtUsername";
+            // 
+            // lblLogin
+            // 
+            resources.ApplyResources(this.lblLogin, "lblLogin");
+            this.lblLogin.Name = "lblLogin";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // FormLogin
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pnlLogin2);
+            this.Controls.Add(this.pnlLogin1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormLogin";
+            this.pnlLogin1.ResumeLayout(false);
+            this.pnlLogin1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlLogin2.ResumeLayout(false);
+            this.pnlLogin2.PerformLayout();
+            this.pnlCabain.ResumeLayout(false);
+            this.pnlCabain.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlLogin1;
+        private System.Windows.Forms.Panel pnlLogin2;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Panel pnlUsername;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Panel pnlPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel pnlCabain;
+        private System.Windows.Forms.Label lblCabain;
+        private System.Windows.Forms.ComboBox cboCabain;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ bdPooFinalPj/bdPooFinalPj/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bdPooFinalPj
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            
+            InitializeComponent();
+
+            txtPassword.PasswordChar = '*';
+
+            int xC = 0;
+            int yC = 0;
+
+            xC = (pnlCabain.Width / 2) - (lblCabain.Width / 2);
+            yC = (pnlCabain.Height / 2) - (lblCabain.Height / 2);
+
+            lblCabain.Location = new System.Drawing.Point(xC, 10);
+            cboCabain.Location = new System.Drawing.Point(xC + 9, 40);
+
+            
+
+        }
+    }
+}
